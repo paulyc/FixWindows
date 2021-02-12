3b453771d18eeff15da1dd63ec518f82aff8d4a6
Paul Ciarlo <paulyc@quetzalcoatl.us>
2021-02-11 20:23:40 -0500

    Update FixWindows.bat

--- FixWindows.bat
+++ FixWindows.bat
@@ -1,5 +1,13 @@
-:: Disable hibernation (saves disk space equal to half RAM)
-:: Why is hibernation still a thing here in the future, when CPU is free and unlimited, and SSD and RAM are not?
-:: If anything it takes even my ancient machine longer to boot from hibernation than fresh, because of all the apps
-:: I didn't still want to use being loaded back into RAM in addition to the ones I did! Also less crashy and leak prone
+:: Run as Administrator. Just like everything else.
+
+:: Disable hibernation (saves disk space equal to half RAM, and RAM equal to
+:: however much every program you had running would have leaked if you didn't restart it)
 powercfg -h off
+
+:: Disable Windows 10 Reserve Storage. Because you're going to run out of SSD space long before Windows ever does.
+DISM.exe /Online /Set-ReservedStorageState /State:Disabled
+
+:: Probably disable the Antimalware "Service" (I didn't sign up for that kind of "service" thanks),
+:: but it at least seems much less obnoxious on recent Win10 builds than it used to be
+
+:: REG QUERY/ADD/DELETE :: We're going to need it
