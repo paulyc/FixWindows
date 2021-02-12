20b512563ed84835f5f5871220a628b5d9596ed4
Paul Ciarlo <paulyc@quetzalcoatl.us>
2021-02-11 20:14:16 -0500

    Update FixWindows.bat

--- FixWindows.bat
+++ FixWindows.bat
@@ -1,5 +1,5 @@
 :: Disable hibernation (saves disk space equal to half RAM)
 :: Why is hibernation still a thing here in the future, when CPU is free and unlimited, and SSD and RAM are not?
 :: If anything it takes even my ancient machine longer to boot from hibernation than fresh, because of all the apps
-:: I didn't still want to use being loaded back into RAM in addition to the ones I did!
+:: I didn't still want to use being loaded back into RAM in addition to the ones I did! Also less crashy and leak prone
 powercfg -h off
