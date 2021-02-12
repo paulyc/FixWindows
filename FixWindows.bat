d65e4859ddc59b799f6509638da514d179e9f5fb
Paul Ciarlo <paulyc@quetzalcoatl.us>
2021-02-11 20:11:43 -0500

    Update FixWindows.bat

--- FixWindows.bat
+++ FixWindows.bat
@@ -1,2 +1,3 @@
 :: Disable hibernation (saves disk space equal to half RAM)
+:: Why is hibernation still a thing here in the future, when CPU is free and unlimited, and SSD and RAM are not?
 powercfg -h off
